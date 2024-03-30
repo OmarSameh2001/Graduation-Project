60c0c7b9678255d3eae135186f073d8910e8d49b
OmarSameh <omarsameh20017@gmail.com>
2024-03-31 01:51:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,175 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
+  const [images, setImages] = useState(null);
+  const [apiResponse, setApiResponse] = useState(null); // State to store API response
+  const [errorRes, setErrorRes] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const apikey = process.env.REACT_APP_API_KEY;
+  console.log(apikey);
+  async function api(image) {
+    try {
+      setLoading(true);
+      const response = await axios({
+        method: "POST",
+        url: "https://classify.roboflow.com/tires-defects/1",
+        params: {
+          api_key: apikey,
+        },
+        data: image,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+      console.log(response.data);
+      setLoading(false);
+      setApiResponse(response.data); // Set API response to state
+    } catch (error) {
+      console.log(error.message);
+      setErrorRes(error.message);
+      setLoading(false);
+    }
+  }
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      const base64String = reader.result
+        .replace("data:", "")
+        .replace(/^.+,/, "");
+      setImages(base64String);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  function hand() {
+    api(images);
+    //apiup(images);
+  }
+  function resetStates() {
+    setApiResponse(null);
+    setImages(null);
+    setErrorRes(null);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <Navbar/>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="App-header"
+    >
+      {!images && (
+        <>
+          <h1 className="py-5">Kindly upload an image</h1>
+          <input
+            style={{ cursor: "pointer" }}
+            type="file"
+            accept="image/*"
+            onChange={handleFileInputChange}
+          />
+        </>
+      )}
+      {images && !apiResponse && !errorRes && (
+        <h1 className="py-2">Press Upload to process your image</h1>
+      )}
+      {images && apiResponse && (
+        <h1 className="py-2">Press reset to test new image</h1>
+      )}
+      {images && errorRes && <h1 className="py-2">Error occured try uploading again or press reset</h1>}
+      {images && (
+        <div
+          style={{
+            marginTop: "20px",
+            maxWidth: "500px",
+            height: "400px",
+            borderRadius: "8px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src={`data:image/jpeg;base64,${images}`}
+            alt="Uploaded"
+            style={{ maxWidth: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </div>
+      )}
+      {apiResponse ? (
+        <div style={{ marginTop: "20px" }} className="row">
+          <p className="col-auto">
+            Your Tire is: {apiResponse.top}, with confidence{" "}
+            {(parseFloat(apiResponse.confidence) * 100).toFixed(2)}%
+          </p>
+
+          {apiResponse.top === "Defected" && (
+            <a
+              href="https://www.fitandfix.com"
+              className="col-auto bg-success text-white"
+              target="_blank"
+              rel="noreferrer"
+              style={{
+                fontSize: "20px",
+                color: "green",
+              }}
+            >
+              Redirect to specialist
+            </a>
+          )}
+
+          <button
+            style={{ margin: "20px", maxWidth: "70%" }}
+            className="btn bg-danger text-white col-md4"
+            onClick={resetStates}
+          >
+            Reset
+          </button>
+        </div>
+      ) : (
+        <button
+          style={{ marginTop: "20px" }}
+          className="btn bg-success text-white upload-button col-md-1"
+          onClick={() => {
+            hand();
+            setErrorRes(null);
+          }}
+        >
+          {loading ? "Loading..." : "Upload"}
+        </button>
+      )}
+      {errorRes === "Request failed with status code 413" ? (
+        <p className="bg-danger mt-3">The image is too large</p>
+      ) : errorRes ? (
+        <><p className="bg-danger mt-3">An error occurred, please try again</p>
+        <button
+            style={{  maxWidth: "70%" }}
+            className="btn bg-danger text-white col-md4"
+            onClick={resetStates}
+          >
+            Reset
+          </button></>
+        
+      ) : (
+        null
+      )}
     </div>
+    <Footer/>
+    </>
+    
+  
   );
 }
 
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="py-4 bg-main-light" style={{justifyContent: "center", textAlign: "center"}}>
+      <div className="container">
+        <h2>Tires Inspector</h2>
+        <h2 className="lead">
+        Empowering Safety through Visual Intelligence.
+        </h2>
+      </div>
+    </footer>
+  );
+}
